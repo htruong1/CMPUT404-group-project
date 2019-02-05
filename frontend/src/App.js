dbfe2e81e23148de020b4f14ea7b849c5ebe063f
hiufungk <hiufungk@ualberta.ca>
2019-02-04 19:20:30 -0500

    installed django, react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './style/assets/logo.svg';
-import './style/css/index/App.css';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
